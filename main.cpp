b44f36c2f23a17584c5da01783e4ace5cfcb1acb
野村優羽 <@MSI>
2023-12-06 15:56:02 +0900

    テスト

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "Engine/Framework/MyFramework.h"
 #include "Engine/Framework/MyGame.h"
 
+// テスト
+
 //Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
