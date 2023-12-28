fdc9eb9cba863292d659c97737e44e07080d8fb2
野村優羽 <@MSI>
2023-12-28 14:02:23 +0900

    中断

--- Application/UI/UI.h
+++ Application/UI/UI.h
@@ -2,6 +2,7 @@
 #include <array>
 #include <memory>
 #include <cstdint>
+#include "../../Engine/2D/Sprite.h"
 
 enum UITextureHandleIndex {
 
@@ -16,15 +17,23 @@ enum UISpriteIndex {
 class UI
 {
 
+public: // サブクラス
+
+	struct UI{
+		std::unique_ptr<Sprite> sprite_;
+	};
+
 public: // メンバ関数
 
-	void Initialize();
+	void Initialize(const std::array<uint32_t, UITextureHandleIndex::kUITextureHandleIndexOfCount>& textureHandles);
 
 private: // メンバ変数
 
 	// テクスチャハンドル
 	std::array<uint32_t, UITextureHandleIndex::kUITextureHandleIndexOfCount> textureHandles_;
 
+	// スプライト
+	std::array<std::unique_ptr<Sprite>, UISpriteIndex::kUISpriteIndexOfCount> sprites_;
 
 };
 
