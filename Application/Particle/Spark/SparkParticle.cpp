d53cdf92d027a9b1d04cad63359673b711c6a444
野村優羽 <@MSI>
2024-01-23 17:51:37 +0900

    スパークパーティクル作成

--- Application/Particle/Spark/SparkParticle.cpp
+++ Application/Particle/Spark/SparkParticle.cpp
@@ -1,10 +1,14 @@
 #include "SparkParticle.h"
+#include <random>
 
 void SparkParticle::Initialize(const Vector3& position, const Vector3& size)
 {
 
 	Vector3Calc* vector3Calc = Vector3Calc::GetInstance();
 	Matrix4x4Calc* matrix4x4Calc = Matrix4x4Calc::GetInstance();
+
+	std::random_device seedGenerator;
+	std::mt19937 randomEngine(seedGenerator());
 
 	transform_.scale = { 1.0f,1.0f,1.0f };
 	transform_.rotate = { 0.0f,0.0f,0.0f };
@@ -26,8 +30,33 @@ void SparkParticle::Initialize(const Vector3& position, const Vector3& size)
 
 	isDead_ = false;
 
+	Vector3 velocityMax = { position.x + size.x / 2.0f,
+								position.y + size.y / 2.0f,
+								position.z + size.z / 2.0f };
+	Vector3 velocityMin = { position.x - size.x / 2.0f,
+								position.y - size.y / 2.0f,
+								position.z - size.z / 2.0f };
+	std::uniform_real_distribution<float> velocityX(velocityMin.x, velocityMax.x);
+	std::uniform_real_distribution<float> velocityY(velocityMin.y, velocityMax.y);
+	std::uniform_real_distribution<float> velocityZ(velocityMin.z, velocityMax.z);
+
+	velocity_ = { velocityX(randomEngine), velocityY(randomEngine), velocityZ(randomEngine) };
+
+	const float pram = 1.0f / lifeTime_ * 60.0f;
+	acceleration_ = { velocity_.x * pram, velocity_.y * pram, velocity_.z * pram };
+
 }
 
 void SparkParticle::Update(const Matrix4x4& billBoardMatrix)
 {
+
+	Vector3Calc* vector3Calc = Vector3Calc::GetInstance();
+	Matrix4x4Calc* matrix4x4Calc = Matrix4x4Calc::GetInstance();
+
+	velocity_ = vector3Calc->Subtract(velocity_, acceleration_);
+
+	transform_.translate = vector3Calc->Add(transform_.translate, velocity_);
+
+	IParticle::Update(billBoardMatrix);
+
 }
