b392e30e9289666dd2766388189f1ae7618d3956
shimazaki-ryuta <k022g0024@g.neec.ac.jp>
2024-01-24 13:52:37 +0900

    クリアシーンへの遷移(中身はタイトルと一緒)

--- Application/Scene/ClearScene/ClearScene.cpp
+++ Application/Scene/ClearScene/ClearScene.cpp
@@ -1,4 +1,12 @@
 #include "ClearScene.h"
+#include "../../../Engine/base/TextureManager.h"
+#include "../../../Engine/2D/ImguiManager.h"
+#include "../../../Engine/GlobalVariables/GlobalVariables.h"
+#include "../../../Engine/Math/Ease.h"
+
+ClearScene::~ClearScene()
+{
+}
 
 void ClearScene::Initialize()
 {
@@ -9,15 +17,112 @@ void ClearScene::Initialize()
 	MaterialCreate();
 	TextureLoad();
 
+	titlePosition_ = { 320.0f, 240.0f };
+	titleTextureHandle_ = TextureManager::Load("Resources/Title/title.png", dxCommon_, textureHandleManager_.get());
+	titleSprite_.reset(Sprite::Create(titleTextureHandle_, titlePosition_, Vector4{ 1.0f, 1.0f, 1.0f, 1.0f }));
+	titleSize_ = titleSprite_->GetSize();
+
+	buttonPosition_ = { 640.0f, 560.0f };
+	buttonTextureHandle_ = TextureManager::Load("Resources/Title/controler_UI_A.png", dxCommon_, textureHandleManager_.get());
+	buttonSprite_.reset(Sprite::Create(buttonTextureHandle_, buttonPosition_, Vector4{ 1.0f, 1.0f, 1.0f, 1.0f }));
+	buttonSprite_->SetTextureSize(Vector2{ 384.0f, 384.0f });
+	buttonSprite_->SetSize(Vector2{ 128.0f, 128.0f });
+	buttonSprite_->SetTextureLeftTop(Vector2{ 0.0f, 0.0f });
+	buttonSize_ = { 128.0f, 128.0f };
+	buttonAlphaT_ = 0.0f;
+	buttonAlphaTSpeed_ = 0.01f;
+	buttonItIncreaseAlphaT_ = true;
+	buttonColor_ = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+	audioManager_ = std::make_unique<TitleAudioManager>();
+	audioManager_->StaticInitialize();
+	audioManager_->Initialize();
+	audioManager_->PlayWave(kTitleAudioNameIndexBGM);
+
+	// ビュープロジェクション
+	TransformStructure baseCameraTransform = {
+		1.0f, 1.0f, 1.0f,
+		0.0f,0.0f,0.0f,
+		0.0f, 0.0f, -35.0f };
+	camera_.SetTransform(baseCameraTransform);
+
+	// スカイドーム
+	skydome_ = std::make_unique<Skydome>();
+	skydome_->Initialize(skydomeModel_.get());
+
+	// プレイヤー
+	player_ = std::make_unique<TitleObj>();
+	player_->Initialize(playerModel_.get(), "Player");
+
+	// ねじ
+	for (uint32_t i = 0; i < screws_.size(); ++i) {
+		screws_[i] = std::make_unique<TitleObj>();
+		screws_[i]->Initialize(screwModel_.get(), "Screw" + std::to_string(i));
+	}
+
+	SpriteRegisteringGlobalVariables();
+
+	SpriteApplyGlobalVariables();
+
+	outline_.Initialize();
+	outline_.color_ = { 0.8f,0.4f,0.1f,1.0f };
+	//アウトライン
+	outline_.Map();
 }
 
 void ClearScene::Update()
 {
 
-	if (input_->TriggerKey(DIK_SPACE)) {
+#ifdef _DEBUG
+	SpriteApplyGlobalVariables();
+#endif // _DEBUG
+
+
+	ImguiDraw();
+
+	if ((input_->TriggerKey(DIK_SPACE) || input_->TriggerJoystick(JoystickButton::kJoystickButtonA)) &&
+		requestSceneNo == kClear) {
 		// 行きたいシーンへ
 		requestSceneNo = kTitle;
+		audioManager_->PlayWave(kTitleAudioNameIndexDecision);
 	}
+
+	// BGM音量下げる
+	if (requestSceneNo == kGame && isDecreasingVolume) {
+		LowerVolumeBGM();
+	}
+
+	// カメラ
+	camera_.Update();
+
+	// スカイドーム
+	skydome_->Update();
+
+	// プレイヤー
+	player_->Update();
+
+	// ねじ
+	for (uint32_t i = 0; i < screws_.size(); ++i) {
+		screws_[i]->Update();
+	}
+
+	// ボタンスプライト
+	if (buttonItIncreaseAlphaT_) {
+		buttonAlphaT_ += buttonAlphaTSpeed_;
+		if (buttonAlphaT_ >= 1.0f) {
+			buttonAlphaT_ = 1.0f;
+			buttonItIncreaseAlphaT_ = false;
+		}
+	}
+	else {
+		buttonAlphaT_ -= buttonAlphaTSpeed_;
+		if (buttonAlphaT_ <= 0.0f) {
+			buttonAlphaT_ = 0.0f;
+			buttonItIncreaseAlphaT_ = true;
+		}
+	}
+	buttonColor_.w = Ease::Easing(Ease::EaseName::Lerp, 0.0f, 1.0f, buttonAlphaT_);
+	buttonSprite_->SetColor(buttonColor_);
 
 }
 
@@ -40,8 +145,25 @@ void ClearScene::Draw()
 
 	//3Dオブジェクトはここ
 
-	Model::PostDraw();
+	// スカイドーム
+	if (isDrawSkydome_) {
+		skydome_->Draw(camera_);
+	}
+	// プレイヤー
+	player_->Draw(camera_);
 
+	// ねじ
+	for (uint32_t i = 0; i < screws_.size(); ++i) {
+		screws_[i]->Draw(camera_);
+	}
+
+	Model::PostDraw();
+	Model::PreDrawOutLine(dxCommon_->GetCommadList());
+
+	for (uint32_t i = 0; i < screws_.size(); ++i) {
+		screws_[i]->DrawOutLine(camera_, outline_);
+	}
+	Model::PostDraw();
 #pragma region 前景スプライト描画
 	// 前景スプライト描画前処理
 	Sprite::PreDraw(dxCommon_->GetCommadList());
@@ -49,6 +171,8 @@ void ClearScene::Draw()
 
 	//背景
 	//前景スプライト描画
+	//titleSprite_->Draw();
+	buttonSprite_->Draw();
 
 	// 前景スプライト描画後処理
 	Sprite::PostDraw();
@@ -57,8 +181,27 @@ void ClearScene::Draw()
 
 }
 
+void ClearScene::ImguiDraw()
+{
+
+#ifdef _DEBUG
+	ImGui::Begin("Title");
+	ImGui::Checkbox("isDrawSkydome", &isDrawSkydome_);
+	ImGui::End();
+#endif // _DEBUG
+
+}
+
 void ClearScene::ModelCreate()
 {
+
+	// スカイドーム
+	skydomeModel_.reset(Model::Create("Resources/Skydome/", "skydome.obj", dxCommon_, textureHandleManager_.get()));
+	// プレイヤー
+	playerModel_.reset(Model::Create("Resources/Model/Player/Original/", "player.obj", dxCommon_, textureHandleManager_.get()));
+	// ねじ
+	screwModel_.reset(Model::Create("Resources/Model/nejimi2_model/", "nejimi2.obj", dxCommon_, textureHandleManager_.get()));
+
 }
 
 void ClearScene::MaterialCreate()
@@ -67,4 +210,71 @@ void ClearScene::MaterialCreate()
 
 void ClearScene::TextureLoad()
 {
+}
+
+void ClearScene::LowerVolumeBGM()
+{
+
+	for (uint32_t i = 0; i < audioManager_->kMaxPlayingSoundData; ++i) {
+		if (audioManager_->GetPlayingSoundDatas()[i].handle_ == kTitleAudioNameIndexBGM) {
+			float decreasingVolume = 1.0f / 60.0f;
+			float volume = audioManager_->GetPlayingSoundDatas()[i].volume_ - decreasingVolume;
+			if (volume < 0.0f) {
+				volume = 0.0f;
+				audioManager_->StopWave(i);
+				isDecreasingVolume = false;
+			}
+			else {
+				audioManager_->SetPlayingSoundDataVolume(i, volume);
+				audioManager_->SetVolume(i, audioManager_->GetPlayingSoundDatas()[i].volume_);
+			}
+		}
+	}
+
+}
+
+void ClearScene::SpriteRegisteringGlobalVariables()
+{
+
+	GlobalVariables* globalVariables = GlobalVariables::GetInstance();
+
+	const std::string groupName = "TilteSceneSprite";
+
+	// タイトル
+	std::string objName = "TilteSprite";
+	globalVariables->AddItem(groupName, objName + "Position", titlePosition_);
+	globalVariables->AddItem(groupName, objName + "Size", titleSize_);
+
+	// ボタン
+	objName = "ButtonSprite";
+	globalVariables->AddItem(groupName, objName + "Position", buttonPosition_);
+	globalVariables->AddItem(groupName, objName + "Size", buttonSize_);
+
+}
+
+void ClearScene::SpriteApplyGlobalVariables()
+{
+
+	GlobalVariables* globalVariables = GlobalVariables::GetInstance();
+
+	const std::string groupName = "TilteSceneSprite";
+
+	// タイトル
+	std::string objName = "TilteSprite";
+
+	titlePosition_ = globalVariables->GetVector2Value(groupName, objName + "Position");
+	titleSprite_->SetPosition(titlePosition_);
+
+	titleSize_ = globalVariables->GetVector2Value(groupName, objName + "Size");
+	titleSprite_->SetSize(titleSize_);
+
+	// ボタン
+	objName = "ButtonSprite";
+
+	buttonPosition_ = globalVariables->GetVector2Value(groupName, objName + "Position");
+	buttonSprite_->SetPosition(buttonPosition_);
+
+	buttonSize_ = globalVariables->GetVector2Value(groupName, objName + "Size");
+	buttonSprite_->SetSize(buttonSize_);
+
 }
--- Application/Scene/ClearScene/ClearScene.h
+++ Application/Scene/ClearScene/ClearScene.h
@@ -1,24 +1,37 @@
 #pragma once
 #include "../../../Engine/Scene/IScene/IScene.h"
+
+#include "../../AudioManager/TitleAudioManager.h"
+
+#include "../../Skydome/Skydome.h"
+#include "../../TitleObj/titleObj.h"
+
 class ClearScene : public IScene
 {
 
 public: // メンバ関数
 
+	~ClearScene();
+
 	/// <summary>
 	/// 初期化
 	/// </summary>
-	virtual void Initialize() override;
+	void Initialize() override;
 
 	/// <summary>
 	/// 更新
 	/// </summary>
-	virtual void Update() override;
+	void Update() override;
 
 	/// <summary>
 	/// 描画
 	/// </summary>
-	virtual void Draw() override;
+	void Draw() override;
+
+	/// <summary>
+	/// imgui描画処理
+	/// </summary>
+	void ImguiDraw();
 
 private: // メンバ関数
 
@@ -37,4 +50,70 @@ private: // メンバ関数
 	/// </summary>
 	void TextureLoad() override;
 
+private:
+
+	/// <summary>
+	/// 音量
+	/// </summary>
+	void LowerVolumeBGM();
+
+	/// <summary>
+	/// 外部変数登録
+	/// </summary>
+	void SpriteRegisteringGlobalVariables();
+
+	/// <summary>
+	/// 外部変数適用
+	/// </summa
+	void SpriteApplyGlobalVariables();
+
+private: // メンバ変数
+
+	// スプライト
+	std::unique_ptr<Sprite> titleSprite_ = nullptr;
+	// テクスチャハンドル
+	uint32_t titleTextureHandle_ = 0u;
+	// ポジション
+	Vector2 titlePosition_ = {};
+	// サイズ
+	Vector2 titleSize_ = {};
+
+	// スプライト
+	std::unique_ptr<Sprite> buttonSprite_ = nullptr;
+	// テクスチャハンドル
+	uint32_t buttonTextureHandle_ = 0u;
+	// ポジション
+	Vector2 buttonPosition_ = {};
+	// サイズ
+	Vector2 buttonSize_ = {};
+	// 点滅用媒介変数
+	float buttonAlphaT_;
+	// 点滅用媒介変数速度
+	float buttonAlphaTSpeed_;
+	// 点滅用媒介変数は増えるか
+	bool buttonItIncreaseAlphaT_;
+	// 色
+	Vector4 buttonColor_;
+
+	// オーディオマネージャー
+	std::unique_ptr<TitleAudioManager> audioManager_;
+
+	bool isDecreasingVolume = true;
+
+	// スカイドーム
+	std::unique_ptr<Skydome> skydome_;
+	std::unique_ptr<Model> skydomeModel_;
+	bool isDrawSkydome_ = true;
+
+	// プレイヤー
+	std::unique_ptr<TitleObj> player_;
+	std::unique_ptr<Model> playerModel_;
+
+	// ねじ
+	std::array<std::unique_ptr<TitleObj>, 3> screws_;
+	std::unique_ptr<Model> screwModel_;
+
+	//アウトライン仮
+	OutLineData outline_;
 };
+
