ba3c7386386fa478126973374843da9a3ac93a3d
shimazaki-ryuta <k022g0024@g.neec.ac.jp>
2024-02-06 16:43:30 +0900

    点線

--- Application/Particle/Liner/LinerEmitter.cpp
+++ Application/Particle/Liner/LinerEmitter.cpp
@@ -28,9 +28,23 @@ std::list<IParticle*> LinerEmitter::Emit()
 	Vector3 translate;
 	Vector3 start = ForLinerEmitterData::GetInstance()->GetStart();
 	Vector3 end = ForLinerEmitterData::GetInstance()->GetEnd();
-	for (uint32_t count = 0; count < instanceCount_; ++count) {
-		translate = Vector3Calc::Lerp(start,end,float(count)/float(instanceCount_-1));
-		particles.push_back(makeParticle->Run(paeticleName_, translate, transform_.scale));
+	Vector3 toVector = Vector3Calc::Normalize(Vector3Calc::Subtract(end, start));
+	float addLength = 2.0f;
+	if (!(start.x == end.x && start.z == end.z)) {
+		float length = Vector3Calc::Length(Vector3Calc::Subtract(end, start));
+		translate = toVector;
+		float length2 = Vector3Calc::Length(toVector);
+		while (length-2.0f > length2) {
+			translate = Vector3Calc::Multiply(addLength,toVector);
+			length2 = Vector3Calc::Length(translate);
+			translate = Vector3Calc::Add(translate,start);
+			particles.push_back(makeParticle->Run(paeticleName_, translate, transform_.scale));
+			addLength+=2.0f;
+		}
+	/*	for (uint32_t count = 0; count < instanceCount_; ++count) {
+			translate = Vector3Calc::Lerp(start, end, float(count) / float(instanceCount_ - 1));
+			particles.push_back(makeParticle->Run(paeticleName_, translate, transform_.scale));
+		}*/
 	}
 
 	return particles;
