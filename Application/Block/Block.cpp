59d668cc366112cd9117b7a90f706176f858bd5e
shimazaki-ryuta <k022g0024@g.neec.ac.jp>
2024-02-07 16:11:01 +0900

    引き寄せoff

--- Application/Block/Block.cpp
+++ Application/Block/Block.cpp
@@ -159,7 +159,7 @@ void Block::OnCollision(ColliderParentObject pairObject, CollisionData collidion
 				break;
 			}
 		}
-		if (isStack) {
+		if (isStack && isConnectableMode_) {
 			Vector3 toPlayer = Vector3Calc::Subtract(std::get<Magnet*>(pairObject)->GetCenter(), worldTransform_.GetWorldPosition());
 			toPlayer.y = 0;
 			toPlayer = Vector3Calc::Normalize(toPlayer);
